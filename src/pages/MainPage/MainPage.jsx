2fbb39c1e80822a061f81ad8337fa95aed5cff58
Alfred Chin <alfredchin2005@gmail.com>
2024-12-03 00:43:28 +0800

    change the tyle of tab for when no group is selected

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -217,7 +217,16 @@ function MainPage() {
           return "chat"
       }
     } else {
-      return <h1>Select a group</h1>;
+      return (
+        <div className="flex flex-col items-center justify-center w-full">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#44C588" className="size-[128px]">
+            <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clipRule="evenodd" />
+          </svg>
+
+          <div className="text-[24px]">It looks empty... Please select or create new group to get start</div>
+        </div>
+
+      ) 
     }
   }
 
